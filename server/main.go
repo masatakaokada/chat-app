4ce731c327dc448d06eb7756f4a976698f46c92c
Masataka Okada <masataka.okada1117@gmail.com>
2022-04-17 16:52:25 +0900

    軽微な修正

--- server/main.go
+++ server/main.go
@@ -7,17 +7,24 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+var e = createMux()
+
 func main() {
-  e := echo.New()
+	e.GET("/", hello)
 
-  e.Use(middleware.Logger())
-  e.Use(middleware.Recover())
+	e.Logger.Fatal(e.Start(":8082"))
+}
 
-  e.GET("/", hello)
+func createMux() *echo.Echo {
+	e := echo.New()
 
-  e.Logger.Fatal(e.Start(":8082"))
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+
+	return e
 }
 
 func hello(c echo.Context) error {
-  return c.String(http.StatusOK, "Hello, World")
+	return c.String(http.StatusOK, "Hello, World")
 }
